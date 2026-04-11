87ec7b9252c9fc7d4368f19bcc52d4aaaa292e86
Navjotsingh09 <navjotsingh09@gmail.com>
2026-04-11 15:10:50 +0100

    Switch placeholders from placehold.co to placeholder.io

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'placehold.co', 'cdn.shopify.com', 'www.crlaurence.co.uk', 'www.egw.co.uk'],
+    domains: ['images.unsplash.com', 'placehold.co', 'placeholder.io', 'cdn.shopify.com', 'www.crlaurence.co.uk', 'www.egw.co.uk'],
     formats: ['image/avif', 'image/webp'],
   },
   async headers() {
